c39d483b5f56696cbc3a18db6a3f1ca138c139e2
Nadejda <bogdanova5439034@gmal>
2021-05-22 19:50:23 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
+/* импорт пакетов */
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass        = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
+/* Статический сервер */
 gulp.task('server', function() {
 
     browserSync({
@@ -14,56 +16,86 @@ gulp.task('server', function() {
             baseDir: "dist"
         }
     });
-
+    
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function() {
+/* компиляция файла из sass в css */
+gulp.task('styles', function(){
+    /* взять файл по определенному адресу */
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe
-        (sass({outputStyle: 'compressed'}).on('error', sass.logError))
+
+        /* компиляция файла */
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+
+        /* переименовать файл */
         .pipe(rename({suffix: '.min', prefix: ''}))
+
+        /* для определения какие префиксы нужно использовать ( -webkit, -ms, -o, -moz)*/
         .pipe(autoprefixer())
+
+        /* минимизация файла */
         .pipe(cleanCSS({compatibility: 'ie8'}))
+
+        /* положить файл по определенному адресу */
         .pipe(gulp.dest("dist/css"))
+
+        /* обновление статического сервера */
         .pipe(browserSync.stream());
 });
 
+/* следить за изменениями в фалах */
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
-});
+    gulp.watch('src/*.html').on("change", gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
+    gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
+    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
+    gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
+})
 
-gulp.task('html', function () {
+
+// сжатие файла .html и перенос в папку dist
+gulp.task('html', function() {
     return gulp.src("src/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest("dist/"));
 });
 
-gulp.task('scripts', function () {
+// сжатие скриптовых файлов и перенос в папку dist
+gulp.task('scripts', function() {
     return gulp.src("src/js/**/*.js")
-        .pipe(gulp.dest("dist/js"));
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('fonts', function () {
+// сжатие файлов fonts и перенос в папку dist
+gulp.task('fonts', function() {
     return gulp.src("src/fonts/**/*")
-        .pipe(gulp.dest("dist/fonts"));
+        .pipe(gulp.dest("dist/fonts"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('icons', function () {
+// сжатие файлов icons и перенос в папку dist
+gulp.task('icons', function() {
     return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"));
+        .pipe(gulp.dest("dist/icons"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('mailer', function () {
+gulp.task('mailer', function() {
     return gulp.src("src/mailer/**/*")
-        .pipe(gulp.dest("dist/mailer"));
+        .pipe(gulp.dest("dist/mailer"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('images', function () {
+// сжатие файлов images и перенос в папку dist
+gulp.task('images', function() {
     return gulp.src("src/img/**/*")
         .pipe(imagemin())
-        .pipe(gulp.dest("dist/img"));
+        .pipe(gulp.dest("dist/img"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'mailer', 'html', 'images'));
+/* запуск всех задач одной задачей */
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'mailer', 'html', 'scripts', 'fonts', 'icons', 'images'));
