62283cacd9c492e3ce0b41852798b81020941348
Nadejda <bogdanovavenera30@gmail.com>
2021-05-23 19:22:03 +0300

    finised all section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,96 @@
+$(document).ready(function() {
+    // modal
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow')
+    $('#thanks').fadeOut();
+  });
+
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks').fadeOut('slow')
+  });
+
+
+    // dawnload
+    $('input[type="file"]').change(function(){
+        var value = $("input[type='file']").val();
+        $('.value').text(value);
+    });
+
+    // up
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1500) {
+          $('.pageup').fadeIn('slow');
+        } else {
+          $('.pageup').fadeOut('slow');
+        }
+    });
+    
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find('input').val("");
+          $('#consultation').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+    
+          $('form').trigger('reset');
+        });
+        return false;
+      });
+});$(document).ready(function() {
+    // modal
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow')
+    $('#thanks').fadeOut();
+  });
+
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks').fadeOut('slow')
+  });
+
+
+    // dawnload
+    $('input[type="file"]').change(function(){
+        var value = $("input[type='file']").val();
+        $('.value').text(value);
+    });
+
+    // up
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1500) {
+          $('.pageup').fadeIn('slow');
+        } else {
+          $('.pageup').fadeOut('slow');
+        }
+    });
+    
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find('input').val("");
+          $('#consultation').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+    
+          $('form').trigger('reset');
+        });
+        return false;
+      });
+});
 
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
